77be40686f894da89fb6e5abb3c9deb628e8d603
Filip Janješić <filip.janjesic@gmail.com>
2024-03-03 16:08:38 +0100

    update

---
+++ DronePilotAcademy/Common/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using DronePilotAcademy.Services;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+
+
+namespace DronePilotAcademy.Common
+{
+    public class Menu
+    {
+        public Menu() 
+        {
+
+        }
+    }
+}
---
+++ DronePilotAcademy/Program.cs
@@ -0,0 +1,3 @@
+using DronePilotAcademy.Common;
+
+new Menu();
