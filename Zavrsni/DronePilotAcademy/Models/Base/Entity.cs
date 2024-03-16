d09c8a72c86681da55908cf6a199075dc292d56c
Filip-Janjesic <filip.janjesic@gmail.com>
2024-03-16 09:43:49 +0100

    Update Entity.cs

---
+++ Zavrsni/DronePilotAcademy/Models/Base/Entity.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DronePilotAcademy.Models.Base
+{
+    public abstract class Entity
+    {
+        public int ID { get; set; }
+    }
+}
