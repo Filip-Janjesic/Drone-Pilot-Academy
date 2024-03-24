bed7fa389ece1e16b4cedeafe45d4b9eb98b22c7
Filip-Janjesic <filip.janjesic@gmail.com>
2024-03-24 21:47:56 +0100

    update

--- Zavrsni/DPAstart/DronePilotAcademyWebApi/Program.cs
+++ Zavrsni/DPAstart/DronePilotAcademyWebApi/Program.cs
@@ -1,35 +1,46 @@
-using System.Text.Json.Serialization;
+using DronePilotAcademyWebApi.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
-var builder = WebApplication.CreateSlimBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.ConfigureHttpJsonOptions(options =>
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(opcije =>
 {
-    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+    opcije.AddPolicy("CorsPolicy",
+        builder =>
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 });
 
+builder.Services.AddDbContext<Context>(o =>
+    o.UseSqlServer(
+        builder.Configuration.
+        GetConnectionString(name: "Context")
+        )
+    );
 var app = builder.Build();
 
-var sampleTodos = new Todo[] {
-    new(1, "Walk the dog"),
-    new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
-    new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-    new(4, "Clean the bathroom"),
-    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
-};
-
-var todosApi = app.MapGroup("/todos");
-todosApi.MapGet("/", () => sampleTodos);
-todosApi.MapGet("/{id}", (int id) =>
-    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
-        ? Results.Ok(todo)
-        : Results.NotFound());
-
-app.Run();
-
-public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
-
-[JsonSerializable(typeof(Todo[]))]
-internal partial class AppJsonSerializerContext : JsonSerializerContext
+app.UseSwagger(options =>
 {
+    options.SerializeAsV2 = true;
+});
+app.UseSwaggerUI(options =>
+{
+    options.ConfigObject.
+    AdditionalItems.Add("requestSnippetsEnabled", true);
 
-}
+});
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+app.UseCors("CorsPolicy");
+app.UseDefaultFiles();
+app.UseDeveloperExceptionPage();
+app.MapFallbackToFile("index.html");
+
+app.UseStaticFiles();
+app.Run();
