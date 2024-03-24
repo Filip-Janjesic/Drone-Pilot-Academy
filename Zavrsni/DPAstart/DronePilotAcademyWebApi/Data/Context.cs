14ac5b6ed0af97af956e1d6dc11e61dcde8189f9
Filip-Janjesic <filip.janjesic@gmail.com>
2024-03-24 21:51:31 +0100

    updates

---
+++ Zavrsni/DPAstart/DronePilotAcademyWebApi/Data/Context.cs
@@ -0,0 +1,44 @@
+﻿using DronePilotAcademyWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DronePilotAcademyWebApi.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Instructor> Instructor { get; set; }
+
+        public DbSet<Drone> Drone { get; set; }
+
+        public DbSet<Category> Category { get; set; }
+
+        public DbSet<Student> Student { get; set; }
+
+        public  DbSet<Course>Course { get; set; }
+
+        protected override void OnModelCreating(
+           ModelBuilder modelBuilder)
+        {
+            //  1:n
+            modelBuilder.Entity<Course>().HasOne(c => c.Drone);
+            modelBuilder.Entity<Course>().HasOne(c => c.Instructor);
+            modelBuilder.Entity<Course>().HasOne(c => c.Category);
+
+            // n:n
+            modelBuilder.Entity<Course>()
+                .HasMany(c => c.Students)
+                .WithMany(s => s.Courses)
+                .UsingEntity<Dictionary<string, object>>("STUDENT_COURSE",
+                sc => sc.HasOne<Student>().WithMany().HasForeignKey("ID_student"),
+                sc => sc.HasOne<Course>().WithMany().HasForeignKey("ID_course"),
+                sc => sc.ToTable("STUDENT_COURSE")
+                );
+        }
+        
+    }
+}
