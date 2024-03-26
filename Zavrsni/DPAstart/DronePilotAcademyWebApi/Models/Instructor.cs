703cbcfb1883d73f65af0ff7ae8fbbcb7bebd4a2
Filip-Janjesic <filip.janjesic@gmail.com>
2024-03-26 16:32:02 +0100

    Create Instructor.cs

---
+++ Zavrsni/DPAstart/DronePilotAcademyWebApi/Models/Instructor.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace DronePilotAcademyWebApi.Models
+{
+    public class Instructor : ENT
+    {
+        public string? FIRST_NAME { get; set; }
+        public string? LAST_NAME { get; set; }
+        public string? PILOT_LICENSE_NUMBER { get; set; }
+        public string? EMAIL { get; set; }
+        public string? CONTACT_NUMBER { get; set; }
+    }
+}
