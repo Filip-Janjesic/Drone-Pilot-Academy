0eb00ebf9f7af7bceb8c1f12e41ac820098da067
Filip-Janjesic <filip.janjesic@gmail.com>
2024-03-26 16:32:00 +0100

    Create Student.cs

---
+++ Zavrsni/DPAstart/DronePilotAcademyWebApi/Models/Student.cs
@@ -0,0 +1,33 @@
+﻿namespace DronePilotAcademyWebApi.Models
+{
+    public class Student : ENT
+    {
+        public string? FIRST_NAME { get; set; }
+        public string? LAST_NAME { get; set; }
+        public string? ADDRESS { get; set; }
+        public string? OIB { get; set; }
+        public string? CONTACT_NUMBER { get; set; }
+        public DateTime DATE_OF_ENROLLMENT { get; set; }
+
+        public ICollection<Course> Courses { get; } = new List<Course>();
+
+
+        enum Status
+        {
+            Withdraw = -1,              
+            ListeningTR = 0,            
+            WaitingTRTest = 1,          
+            LiFirstAid = 2,             
+            WaFirstAidTest = 3,         
+            WaDrivingLessons = 4,       
+            Driving = 5,                
+            WaDrivingLessonsTest = 6,   
+            WaHAK = 7,                  
+            PassedAll = 8               
+        }
+
+
+
+
+    }
+}
