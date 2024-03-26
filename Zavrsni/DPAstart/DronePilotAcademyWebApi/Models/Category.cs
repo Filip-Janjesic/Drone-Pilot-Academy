9ccbcf9b68a13842c82c899013d47c25d2ec03c0
Filip-Janjesic <filip.janjesic@gmail.com>
2024-03-26 16:32:08 +0100

    Create Category.cs

---
+++ Zavrsni/DPAstart/DronePilotAcademyWebApi/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace DronePilotAcademyWebApi.Models
+{
+    public class Category : ENT
+    {
+        
+
+        public string? NAME { get; set; }
+        public decimal PRICE { get; set; }
+        public string?  NUMBER_OF_TR_LECTURES { get; set; }
+        public string? NUMBER_OF_FLYING_LECTURES { get; set; }
+
+       
+    }
+}
