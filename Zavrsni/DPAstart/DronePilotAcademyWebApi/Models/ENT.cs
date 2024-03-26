f195cefee969be0df24407c929307f72f0d6610f
Filip-Janjesic <filip.janjesic@gmail.com>
2024-03-26 16:32:03 +0100

    Create ENT.cs

---
+++ Zavrsni/DPAstart/DronePilotAcademyWebApi/Models/ENT.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DronePilotAcademyWebApi.Models
+{
+    public class ENT
+    {
+        [Key]
+        public int ID { get; set; }
+    }
+}
