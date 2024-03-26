e4c24339112ef6f0be3f9d8f254d1a5da1204a7f
Filip-Janjesic <filip.janjesic@gmail.com>
2024-03-26 16:32:05 +0100

    Create Drone.cs

---
+++ Zavrsni/DPAstart/DronePilotAcademyWebApi/Models/Drone.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace DronePilotAcademyWebApi.Models
+{
+    public class Vehicle : ENT
+    {
+        public string TYPE { get; set; }
+        public string? BRAND { get; set; }
+        public string? MODEL { get; set; }
+        public DateTime? PURCHASE_DATE { get; set; }
+        public DateTime? DATE_OF_REGISTRATION { get; set; }
+    }
+}
