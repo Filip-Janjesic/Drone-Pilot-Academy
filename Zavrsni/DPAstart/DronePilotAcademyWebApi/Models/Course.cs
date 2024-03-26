41ad7e602da882afcaa356dc57efaeaf63f3f216
Filip-Janjesic <filip.janjesic@gmail.com>
2024-03-26 16:32:06 +0100

    Create Course.cs

---
+++ Zavrsni/DPAstart/DronePilotAcademyWebApi/Models/Course.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace DronePilotAcademyWebApi.Models
+{
+    public class Course : ENT
+    {
+
+
+        [ForeignKey("ID_Instructor")]
+        public  Instructor? Instructor { get; set; }
+
+
+        [ForeignKey("ID_Drone")]
+        public Drone? Drone { get; set; }
+
+
+        [ForeignKey("ID_Category")]
+        public Category? Category { get; set; }
+
+
+        public  DateTime? START_DATE { get; set; }
+
+        public List<Student> Students { get; set; } = new();
+
+
+
+
+    }
+}
